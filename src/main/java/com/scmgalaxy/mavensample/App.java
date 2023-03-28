a540b3c0db4b4585c43ae91bc0cf415bb7a92463
sh1f2na <128377021+sh1f2na@users.noreply.github.com>
2023-03-28 10:58:33 +0530

    JIR-2
    successfully committed

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello super worldd - login issue fix" );
+        System.out.println( "Hello devops  worldd - login issue fix" );
+        System.out.println( "Hello jira  - login issue fix" );
     }
 }
